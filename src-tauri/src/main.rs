bf356645a57572d5ac17d63291f0ae96d58a5162
Danhalf <danhel@rocketmail.com>
2023-09-04 11:59:42 +0300

    added: db queries

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,36 +4,57 @@ extern crate rusqlite;
 
 use rusqlite::{Connection, Result, params};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn my_custom_command() -> String {
+  "Hello from Rust!".into()
 }
 
 fn main() -> Result<()> {
-    tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
-
-
     let conn = Connection::open("cars.db")?;
 
     conn.execute(
-        "create table if not exists cats (
-             id integer primary key,
-             car_name text not null,
-             car_number text not null
-             odometer number
-
+        "CREATE TABLE IF NOT EXISTS cars (
+            id INTEGER PRIMARY KEY,
+            car_name TEXT NOT NULL,
+            car_number TEXT NOT NULL UNIQUE, 
+            fuel_consumption_main NUMBER NOT NULL,
+            fuel_consumption_other NUMBER
          )",
         params![],
     )?;
 
+      // Розпочніть транзакцію
+      conn.execute("BEGIN", params![])?;
+
+      if let Err(err) = conn.execute(
+          "INSERT INTO cars (car_name, car_number, fuel_consumption_main, fuel_consumption_other)
+           VALUES (?, ?, ?, ?)",
+          params![
+              "Toyota Camry",
+              "ABC123",
+              8.5,
+              6.0, // Інші дані, які ви хочете вставити
+          ],
+      ) {
+          eprintln!("Помилка під час вставки даних: {:?}", err);
+          // Відкатити транзакцію у разі помилки
+          conn.execute("ROLLBACK", params![])?;
+      } else {
+          // Закінчити транзакцію, якщо все пройшло успішно
+          conn.execute("COMMIT", params![])?;
+      }
+
+    tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![my_custom_command])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+
     Ok(())
 }
 
-// tauri::Builder::default()
-//     .invoke_handler(tauri::generate_handler![greet])
-//     .run(tauri::generate_context!())
-//     .expect("error while running tauri application");
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
